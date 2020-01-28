3e430556e93b1e65982df3e803c57f9f30bb5bbd
Nils Rehwald <nils.rehwald@mutelcor.com>
2020-01-28 17:03:48 +0100

    Some Fixes

--- tictactoe.c
+++ tictactoe.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+char spieler1[30];
+char spieler2[30];
+char zeichenS1;
+char zeichenS2;
+
+int main() {
+	printf("Willkommen zu Tic-Tac-Toe\n");
+	char spielfeld[3][3];
+	for(int i=0; i<3; i++) {
+		for(int j=0; j<3; j++) {
+			spielfeld[i][j]=' ';
+		}
+	}
+	printf("Spieler 1, bitte geben sie ihren Namen ein: ");
+	scanf("%99s", &spieler1[0]);
+	printf("Spieler 1, bitte geben sie ihr Zeichen ein: ");
+	scanf(" %c", &zeichenS1);
+	printf("Spieler 2, bitte geben sie ihren Namen ein: ");
+	scanf("%99s", &spieler2[0]);
+	printf("Spieler 2, bitte geben sie ihr Zeichen ein: ");
+	scanf(" %c", &zeichenS2);
+	printf("Lasset die Spiele Beginnen. ");
+	int spielerAmZug = 0;
+	srand (time (NULL));
+	if((rand () % ((99 + 1) - 1)) + 1 > 50) {
+		spielerAmZug=1;
+		printf("Spieler %s beginnt!\n",spieler1);
+	} else {
+		spielerAmZug=2;
+		printf("Spieler %s beginnt!\n",spieler2);
+	}
+
+	
+	spielfeldzeigen(spielfeld);
+
+	spielzug(spielfeld,1);
+	spielzug(spielfeld,2);
+	spielzug(spielfeld,1);	
+
+	spielfeldzeigen(spielfeld);
+}
 
 void spielfeldzeigen(char spielfeld[3][3]) {
 	printf(" ");
@@ -9,7 +53,7 @@ void spielfeldzeigen(char spielfeld[3][3]) {
 	}
 	printf("\n");
 	for(int i=0; i<3; i++) {
-		printf("%d",i);
+		printf("%d",i+1);
 		for(int j=0; j<3; j++) {
 			printf("%c",spielfeld[i][j]);
 		}
@@ -18,22 +62,12 @@ void spielfeldzeigen(char spielfeld[3][3]) {
 	printf("\n\n");
 }
 
-
-int main() {
-	printf("Willkommen zu Tic-Tac-Toe\n");
-	char *spielfeld[3][3];
-	for(int i=0; i<3; i++) {
-		for(int j=0; j<3; j++) {
-			spielfeld[i][j]='o';
-		}
-	}
-	spielfeldzeigen(spielfeld);
-	spielzug(spielfeld,1);
-	spielfeldzeigen(spielfeld);
-}
-
 void spielzug(char spielfeld[3][3], int spieler) {
-	printf("Spieler %d, bitte waehle eine Koordinate fuer deinen Zug: ");
+	if(spieler==1) {
+		printf("Spieler %s, bitte waehle eine Koordinate fuer deinen Zug: ",spieler1);
+	} else {
+		printf("Spieler %s, bitte waehle eine Koordinate fuer deinen Zug: ",spieler2);
+	}
 	char coord[2];
 	scanf("%s",&coord);
 	int x;
@@ -48,14 +82,22 @@ void spielzug(char spielfeld[3][3], int spieler) {
 	else if(coord[0] == 'C' || coord[0] == 'C') {
 		x =  3;
 	}
+	else {
+		printf("Invalid x-Input %c. Aborting Move",coord[0]);
+		return 0;
+	}
+	if(y>3 || y<1) {
+		printf("Invalid y-Input %c. Aborting Move",coord[1]);
+		return 0;
+	}
 	x--;
 	y--;
 	char zeichen;
 	if(spieler == 1) {
-		zeichen = 'o';
+		zeichen = zeichenS1;
 	}
 	else {
-		zeichen = 'x';
+		zeichen = zeichenS2;
 	}
-	spielfeld[x][y] = &zeichen;
+	spielfeld[x][y] = zeichen;
 }
